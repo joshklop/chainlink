ea7bb4bfeebbea4cec111eb6b009ae8abf4923e8
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2020-08-12 18:54:31 +0100

    tmp - fix vrf test

--- core/store/models/vrf_coordinator_interface.go
+++ core/store/models/vrf_coordinator_interface.go
@@ -74,12 +74,13 @@ func (l *RandomnessRequestLog) Equal(ol RandomnessRequestLog) bool {
 func RawRandomnessRequestLogToRandomnessRequestLog(
 	l *RawRandomnessRequestLog) *RandomnessRequestLog {
 	return &RandomnessRequestLog{
-		KeyHash: l.KeyHash,
-		Seed:    l.Seed,
-		JobID:   l.JobID,
-		Sender:  l.Sender,
-		Fee:     (*assets.Link)(l.Fee),
-		Raw:     *l,
+		KeyHash:   l.KeyHash,
+		Seed:      l.Seed,
+		JobID:     l.JobID,
+		Sender:    l.Sender,
+		Fee:       (*assets.Link)(l.Fee),
+		RequestID: l.RequestID,
+		Raw:       *l,
 	}
 }
 
